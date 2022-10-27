2bfaf6a4c842b5fb48b7d4fa49ebe7aeb29835d4
HABIBA0303 <rofola22@gmail.com>
2022-10-27 17:02:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors')
+
+const catagory = require('./catagory.json')
+app.get('/catagory', (req, res) => {
+    res.send(catagory)
+})
+
+const course = require('./item.json')
+app.get('/course', (req, res) => {
+    res.send(course)
+})
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello !')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app;
