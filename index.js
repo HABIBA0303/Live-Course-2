92e284da6bc7faedc5968376acd55220a6adef89
HABIBA0303 <rofola22@gmail.com>
2022-10-28 23:12:10 +0600

    catagory updated

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-const cors = require('cors')
+
+app.use(cors());
 
 const catagory = require('./catagory.json')
+
+
+app.get('/', (req, res) => {
+    res.send('Hello !')
+})
+
 app.get('/catagory', (req, res) => {
     res.send(catagory)
 })
 
-const course = require('./item.json')
-app.get('/course', (req, res) => {
+const course = require('./item.json');
+
+app.get('/courses', (req, res) => {
     res.send(course)
 })
+const courseDetails = require('./courseDetails.json')
 
-app.use(cors())
-app.get('/', (req, res) => {
-    res.send('Hello !')
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const details = courseDetails.find(n => n.id == id);
+    res.send(details)
 })
+const checkOutItems = require('./checkOutItems.json');
+
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id;
+    const checkOut = checkOutItems.find(n => n.id == id);
+    res.send(checkOut)
+})
+
 
 
 app.listen(port, () => {
